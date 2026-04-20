a8c2b6e129a1c253caf4a970b14fffdff756c646
Mick Becker <65152102+bickmeck@users.noreply.github.com>
2026-04-20 14:00:34 +0200

    Enhance install script with oh-my-posh setup
    Added installation steps for oh-my-posh and its configuration.

--- install.sh
+++ install.sh
@@ -4,13 +4,25 @@ zshrc() {
     echo "==========================================================="
     echo "             softlink aliases.zsh                          "
     echo "-----------------------------------------------------------"
-# Get the directory in which this script lives.
     script_dir=$(dirname "$(readlink -f "$0")")
     ln -s $script_dir/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh
+
     echo "==========================================================="
     echo "             cloning zsh-autosuggestions                   "
     echo "-----------------------------------------------------------"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+    echo "==========================================================="
+    echo "             installing oh-my-posh                         "
+    echo "-----------------------------------------------------------"
+    curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+
+    echo "==========================================================="
+    echo "             copying oh-my-posh config                     "
+    echo "-----------------------------------------------------------"
+    mkdir -p ~/.config/oh-my-posh
+    cp "$script_dir/oh-my-posh-config.omp.json" ~/.config/oh-my-posh/config.omp.json
+
     echo "==========================================================="
     echo "                 Copy .zshrc to HOME                       "
     echo "-----------------------------------------------------------"
